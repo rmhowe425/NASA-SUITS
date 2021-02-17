0587970b066abaa4a36d7cdcca1279b2a1c09986
Romogna <alderon13@gmail.com>
2021-02-16 17:50:57 -0700

    Minimize Menu
    - Button to minimize menu and bring it back
    - Handpalmup Solver added with navigation menu
    - Adjusted min distance of RadialView Solver

---
+++ InitialSetup/Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+    // Toggle function
+    public GameObject TheButton;
+
+    public void togglePanel()
+    {
+        if (TheButton != null)
+        {
+            bool isActive = TheButton.activeSelf;
+
+            TheButton.SetActive(!isActive);
+        }
+    }
+}
