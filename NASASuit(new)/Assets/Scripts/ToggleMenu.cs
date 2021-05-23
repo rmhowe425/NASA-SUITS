95d929f361ff1b1905650f5bff03745990903b55
catshirtguy <emland2@outlook.com>
2021-05-23 11:47:26 -0700

    Updated toggle on/off to include Geology Panel

--- NASASuit(new)/Assets/Scripts/ToggleMenu.cs
+++ NASASuit(new)/Assets/Scripts/ToggleMenu.cs
@@ -8,6 +8,7 @@ public class ToggleMenu : MonoBehaviour
     public GameObject DestinationMenu;
     public GameObject MarkerMenu;
     public GameObject LightingMenu;
+    public GameObject Geo_Panel;
     //public GameObject SpareMenu;
 
     int caseSwitch = 0;
@@ -22,21 +23,31 @@ public class ToggleMenu : MonoBehaviour
                 DestinationMenu.SetActive(true);
                 MarkerMenu.SetActive(false);
                 LightingMenu.SetActive(false);
+                Geo_Panel.SetActive(false);
                 break;
             case 2:
                 DestinationMenu.SetActive(false);
                 MarkerMenu.SetActive(true);
                 LightingMenu.SetActive(false);
+                Geo_Panel.SetActive(false);
                 break;
             case 3:
                 DestinationMenu.SetActive(false);
                 MarkerMenu.SetActive(false);
                 LightingMenu.SetActive(true);
+                Geo_Panel.SetActive(false);
                 break;
             case 4:
                 DestinationMenu.SetActive(false);
                 MarkerMenu.SetActive(false);
                 LightingMenu.SetActive(false);
+                Geo_Panel.SetActive(true);
+                break;
+            case 5:
+                DestinationMenu.SetActive(false);
+                MarkerMenu.SetActive(false);
+                LightingMenu.SetActive(false);
+                Geo_Panel.SetActive(false);
                 break;
             default:
                 break;
